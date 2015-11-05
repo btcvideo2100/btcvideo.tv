73a91cde31d06d93f8c0a1d5d9ce56dc9a000bae
btcvideo2100 <btcvideo2100@gmail.com>
2015-11-06 00:25:54 +0800

    html output

--- public/assets/javascripts/components/email-form.js
+++ public/assets/javascripts/components/email-form.js
@@ -3,12 +3,11 @@ import React from 'react'
 var widget = React.createClass({
   getInitialState: function() {
     return {
-      hidden: false,
+      hidden: true,
       sent: false
     }
   },
   getPaymentLink: function() {
-    // TODO 这里如何根据数据输出不同的HTML
     this.setState({
       sent: true
     })
@@ -21,17 +20,21 @@ var widget = React.createClass({
     }, 3000, this)
   },
   render: function() {
+    var fragment = this.state.sent ? <div>ok!</div> : (
+      <form className="pure-form">
+        <fieldset>
+          <legend>Pay with Bitcoin</legend>
+          <input className="email-form-input" type="email" placeholder="Enter your email to get payment link" required />
+          <button type="button" className="pure-button pure-button-primary" onClick={this.getPaymentLink}>
+            Send payment link
+          </button>
+        </fieldset>
+      </form>
+    )
+
     return (
       <div className="email-form" hidden={this.state.hidden}>
-        <form className="pure-form">
-          <fieldset>
-            <legend>Pay with Bitcoin</legend>
-            <input className="email-form-input" type="email" placeholder="Enter your email to get payment link" required />
-            <button type="button" className="pure-button pure-button-primary" onClick={this.getPaymentLink}>
-              Send payment link
-            </button>
-          </fieldset>
-        </form>
+        {fragment}
       </div>
     )
   }
